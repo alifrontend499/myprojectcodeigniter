86f4417e79e8b7f1b701889cff38eee7f770b3ed
= <=>
2018-04-20 10:07:38 +0530

    office changes

--- application/models/login_model.php
+++ application/models/login_model.php
@@ -5,8 +5,10 @@ class Login_model extends CI_Model
 {
   public function login_check($email, $pass) {
     $check_password = $this->db->select("pass")->where(["email"=> $email])->limit("1")->get("login_details");
+
     $fetched_pass = $check_password->row();
     $new_pass = '';
+
     if (isset($fetched_pass)) {
       $new_pass = $fetched_pass->pass;
     }
@@ -21,8 +23,6 @@ class Login_model extends CI_Model
            $result_data = $value;
          }
          return $result_data;
-       } else {
-         return FALSE;
        }
     } else {
       return FALSE;
